69cbd310e4e5e99b4d661caab4a69654ae0e5769
SRibasDev <samuelribas2233@gmail.com>
2026-06-09 14:44:30 -0300

    feat: adiciona exceptions de PetNotFound e TutorNotFound

--- src/main/java/com/particaolar/mundo/system/exception/PetNotFoundException.java
+++ src/main/java/com/particaolar/mundo/system/exception/PetNotFoundException.java
@@ -1,7 +1,7 @@
 package com.particaolar.mundo.system.exception;
 
 public class PetNotFoundException extends RuntimeException {
-  public PetNotFoundException(String message) {
-    super(message);
-  }
+    public PetNotFoundException(String message) {
+        super(message);
+    }
 }
