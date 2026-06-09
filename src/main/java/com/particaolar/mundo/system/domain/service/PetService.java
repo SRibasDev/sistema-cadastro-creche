e922a126f244b9b880f6016b266cb06c76790ae4
SRibasDev <samuelribas2233@gmail.com>
2026-06-09 14:43:29 -0300

    fix: aprimora exceptions

--- src/main/java/com/particaolar/mundo/system/domain/service/PetService.java
+++ src/main/java/com/particaolar/mundo/system/domain/service/PetService.java
@@ -6,6 +6,7 @@ import com.particaolar.mundo.system.domain.repository.PetRepository;
 import com.particaolar.mundo.system.domain.repository.TutorRepository;
 import com.particaolar.mundo.system.dto.PetRequestDTO;
 import com.particaolar.mundo.system.dto.PetResponseDTO;
+import com.particaolar.mundo.system.exception.PetNotFoundException;
 import com.particaolar.mundo.system.mapper.PetMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -29,27 +30,35 @@ public class PetService {
     }
 
     @Transactional (readOnly = true)
-    public PetResponseDTO buscarPorId(Long id){
-        Pet pet = petRepository.findById(id).orElseThrow(() -> new RuntimeException("Pet não encontrado com o ID: " +id));
+    public PetResponseDTO buscarPetPorPetId(Long petId){
+        Pet pet = petRepository.findById(petId).orElseThrow(() -> new PetNotFoundException(petId));
         return petMapper.toResponseDTO(pet);
     }
 
     @Transactional
     public PetResponseDTO salvar(PetRequestDTO petRequestDTO, Long tutorId){
         Tutor tutor = tutorRepository.findById(tutorId)
-                .orElseThrow(() -> new RuntimeException("Tutor não encontrado"));
+                .orElseThrow(() -> new PetNotFoundException(tutorId));
         Pet pet = petMapper.toEntity(petRequestDTO);
         pet.setTutor(tutor);
-
         Pet petSalvo = petRepository.save(pet);
-
         return petMapper.toResponseDTO(petSalvo);
+    }
+
+    @Transactional
+    public PetResponseDTO atualizar(PetRequestDTO requestDTO, Long petId){
+        Pet pet = petRepository.findById(petId)
+                .orElseThrow(() -> new PetNotFoundException(petId));
+        petMapper.updateEntityFromDTO(requestDTO,pet);
+        Pet petAtualizado = petRepository.save(pet);
+        return petMapper.toResponseDTO(petAtualizado);
+
     }
 
     @Transactional
     public void deletar(Long id){
         Pet pet = petRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Pet não encontrado com o id: " + id));
+                .orElseThrow(() -> new PetNotFoundException(id));
         petRepository.delete(pet);
     }
 }
