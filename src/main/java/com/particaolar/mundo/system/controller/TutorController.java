e1132733ae3dfc15dcf501104a25e77818b33e8b
SRibasDev <samuelribas2233@gmail.com>
2026-06-10 22:49:20 -0300

    feat(controller): adicione TutorController com validações

--- src/main/java/com/particaolar/mundo/system/controller/TutorController.java
+++ src/main/java/com/particaolar/mundo/system/controller/TutorController.java
@@ -1,42 +1,38 @@
 package com.particaolar.mundo.system.controller;
 
-import com.particaolar.mundo.system.domain.entity.Tutor;
 import com.particaolar.mundo.system.domain.service.TutorService;
+import com.particaolar.mundo.system.dto.TutorRequestDTO;
+import com.particaolar.mundo.system.dto.TutorResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tutores") // Esta é a porta de entrada (a URL)
+@RequestMapping("/api/tutores")
 @RequiredArgsConstructor
 public class TutorController {
 
-    // O Garçom (Controller) chama o Chef (Service)
     private final TutorService tutorService;
 
-    // Endpoint 1: CADASTRAR UM TUTOR (Chega um pedido POST)
     @PostMapping
-    public ResponseEntity<Tutor> cadastrar(@RequestBody Tutor tutor) {
-        Tutor tutorSalvo = tutorService.salvarTutor(tutor);
-        // Devolve o status 201 (Criado com Sucesso) e os dados do tutor gerado
-        return ResponseEntity.status(HttpStatus.CREATED).body(tutorSalvo);
+    public ResponseEntity<TutorResponseDTO> cadastrar(@Validated @RequestBody TutorRequestDTO requestDTO) {
+        TutorResponseDTO responseDTO = tutorService.salvarTutor(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
-    // Endpoint 2: LISTAR TODOS OS TUTORES (Chega um pedido GET)
     @GetMapping
-    public ResponseEntity<List<Tutor>> listarTodos() {
-        List<Tutor> tutores = tutorService.listarTodos();
-        // Devolve o status 200 (OK) e a lista
+    public ResponseEntity<List<TutorResponseDTO>> listarTodos() {
+        List<TutorResponseDTO> tutores = tutorService.listarTodos();
         return ResponseEntity.ok(tutores);
     }
 
-    // Endpoint 3: BUSCAR TUTOR POR ID (Chega um pedido GET com o número na URL)
     @GetMapping("/{id}")
-    public ResponseEntity<Tutor> buscarPorId(@PathVariable Long id) {
-        Tutor tutor = tutorService.buscarPorId(id);
-        return ResponseEntity.ok(tutor);
+    public ResponseEntity<TutorResponseDTO> buscarPorId(@PathVariable Long id) {
+        TutorResponseDTO responseDTO = tutorService.buscarPorId(id);
+        return ResponseEntity.ok(responseDTO);
     }
 }
