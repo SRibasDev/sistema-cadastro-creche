81e63a9dd3e50cf49f7dfed4058f9935eb560f6b
SRibasDev <samuelribas2233@gmail.com>
2026-06-10 22:44:20 -0300

    feat: adiciona PetMapper

--- src/main/java/com/particaolar/mundo/system/mapper/PetMapper.java
+++ src/main/java/com/particaolar/mundo/system/mapper/PetMapper.java
@@ -3,12 +3,13 @@ package com.particaolar.mundo.system.mapper;
 import com.particaolar.mundo.system.domain.entity.Pet;
 import com.particaolar.mundo.system.dto.PetRequestDTO;
 import com.particaolar.mundo.system.dto.PetResponseDTO;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PetMapper {
 
-    public Pet toEntity(PetRequestDTO dto) {
+    public Pet toEntity(@NonNull PetRequestDTO dto) {
         Pet petEntity = new Pet();
         petEntity.setNome(dto.nome());
         petEntity.setRaca(dto.raca());
@@ -16,13 +17,18 @@ public class PetMapper {
         return petEntity;
     }
 
-    public PetResponseDTO toResponseDTO(Pet petEntity) {
+    public PetResponseDTO toResponseDTO(@NonNull Pet petEntity) {
         return new PetResponseDTO(
                 petEntity.getId(),
                 petEntity.getNome(),
                 petEntity.getRaca(),
                 petEntity.getDataNascimento()
         );
+    }
+    public void updateEntityFromDTO(@NonNull PetRequestDTO dto, @NonNull Pet pet) {
+        pet.setNome(dto.nome());
+        pet.setRaca(dto.raca());
+        pet.setDataNascimento(dto.dataNascimento());
     }
 }
 
