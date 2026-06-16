5e59bebf2a5186e9281616df12345a235efc6a3d
SRibasDev <samuelribas2233@gmail.com>
2026-06-16 16:20:54 -0300

    fix(mapper): corrige typo no método updateEntityFromDTO do TutorMapper

--- src/main/java/com/particaolar/mundo/system/mapper/TutorMapper.java
+++ src/main/java/com/particaolar/mundo/system/mapper/TutorMapper.java
@@ -4,12 +4,11 @@ import com.particaolar.mundo.system.domain.entity.Tutor;
 import com.particaolar.mundo.system.dto.TutorRequestDTO;
 import com.particaolar.mundo.system.dto.TutorResponseDTO;
 import jakarta.validation.constraints.NotNull;
-import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TutorMapper {
-    public Tutor toEntity(@NonNull TutorRequestDTO dto) {
+    public Tutor toEntity(@NotNull TutorRequestDTO dto) {
         Tutor tutorEntity = new Tutor();
         tutorEntity.setNome(dto.nome());
         tutorEntity.setTelefone(dto.telefone());
@@ -17,15 +16,14 @@ public class TutorMapper {
         return tutorEntity;
     }
 
-    public TutorResponseDTO toResponseDTO(@NonNull Tutor tutorEntity) {
+    public TutorResponseDTO toResponseDTO(@NotNull Tutor tutorEntity) {
         return new TutorResponseDTO(
                 tutorEntity.getId(),
                 tutorEntity.getNome(),
                 tutorEntity.getTelefone()
         );
     }
-
-    public void updateEntitiyFromDTO(@NotNull TutorRequestDTO dto,@NotNull Tutor tutor){
+    public void updateEntityFromDTO(@NotNull TutorRequestDTO dto,@NotNull Tutor tutor){
         tutor.setNome(dto.nome());
         tutor.setTelefone(dto.telefone());
         tutor.setCpf(dto.cpf());
