5322cb3fc36a26c91b97f14c47f982b0a340c1b3
SRibasDev <samuelribas2233@gmail.com>
2026-06-26 11:35:55 -0300

    feat(mapper): implementa conversão entre hospedagem e DTOs

---
+++ src/main/java/com/particaolar/mundo/system/mapper/HospedagemMapper.java
@@ -0,0 +1,38 @@
+package com.particaolar.mundo.system.mapper;
+
+import com.particaolar.mundo.system.domain.entity.Hospedagem;
+import com.particaolar.mundo.system.dto.hospedagemDTO.HospedagemRequestDTO;
+import com.particaolar.mundo.system.dto.hospedagemDTO.HospedagemResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HospedagemMapper {
+
+    public Hospedagem toEntity(HospedagemRequestDTO dto) {
+        Hospedagem hospedagem = new Hospedagem();
+        hospedagem.setDataEntrada(dto.dataEntrada());
+        hospedagem.setDataSaida(dto.dataSaida());
+        hospedagem.setObservacoes(dto.observacoes());
+        return hospedagem;
+    }
+
+    public HospedagemResponseDTO toResponseDTO(Hospedagem hospedagem) {
+        return new HospedagemResponseDTO(
+                hospedagem.getId(),
+                hospedagem.getPet().getId(),
+                hospedagem.getPet().getNome(),
+                hospedagem.getPet().getTutor().getNome(),
+                hospedagem.getDataEntrada(),
+                hospedagem.getDataSaida(),
+                hospedagem.getStatus(),
+                hospedagem.getObservacoes(),
+                hospedagem.getCriadoEm()
+        );
+    }
+
+    public void updateEntityFromDTO(HospedagemRequestDTO dto, Hospedagem hospedagem) {
+        hospedagem.setDataEntrada(dto.dataEntrada());
+        hospedagem.setDataSaida(dto.dataSaida());
+        hospedagem.setObservacoes(dto.observacoes());
+    }
+}
