59050ee9bb37d1d3af0c147e767b1764e1156ba0
SRibasDev <samuelribas2233@gmail.com>
2026-06-27 13:03:50 -0300

    feat: adiciona metodo findByAtivoTrue

--- src/main/java/com/particaolar/mundo/system/domain/repository/PetRepository.java
+++ src/main/java/com/particaolar/mundo/system/domain/repository/PetRepository.java
@@ -1,6 +1,8 @@
 package com.particaolar.mundo.system.domain.repository;
 
 import com.particaolar.mundo.system.domain.entity.Pet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import java.util.List;
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
     List<Pet> findByTutorId(Long tutorId);
+    Page<Pet> findByAtivoTrue(Pageable pageable);
 }
