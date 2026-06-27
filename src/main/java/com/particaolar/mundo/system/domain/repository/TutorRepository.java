0d8cf36a6c970364a67424c4dc7490403d80feb4
SRibasDev <samuelribas2233@gmail.com>
2026-06-27 13:12:06 -0300

    feat: adiciona metodo findByAtivoTrue

--- src/main/java/com/particaolar/mundo/system/domain/repository/TutorRepository.java
+++ src/main/java/com/particaolar/mundo/system/domain/repository/TutorRepository.java
@@ -1,6 +1,8 @@
 package com.particaolar.mundo.system.domain.repository;
 
 import com.particaolar.mundo.system.domain.entity.Tutor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
     boolean existsByCpf(String cpf);
     boolean existsByTelefone(String telefone);
+    Page<Tutor> findByAtivoTrue(Pageable pageable);
 
 }
